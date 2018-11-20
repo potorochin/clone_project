344dcdcec946d33ddd25784e8fbd11250a48b916
Bogdan <dp251196pbv@gmail.com>
2018-11-20 17:58:33 +0200

    Fixed for url git

---
+++ src/aceptor.erl
@@ -0,0 +1,93 @@
+
+-module(aceptor). 
+-behaviour(gen_server).   
+-define(SERVER, ?MODULE). 
+
+
+-export([start_link/0, getMe/0, sendMessage/0, forwardMessage/0,getUpdates/0]).
+
+
+-export([                      
+  init/1,                      
+  handle_call/3,               
+  handle_cast/2,               
+  handle_info/2,               
+  terminate/2,                 
+  code_change/3]).             
+
+start_link() ->          
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+-define(HOST,   "https://api.telegram.org/bot").
+-define(TOKEN,  "702014129:AAEmJdQQhEm8oGrh0Ai3szKUCQqVul-T2qE").
+-define(GETME,  "/getUpdates").
+-define(GETUPDATES,  "/getUpdates").
+-define(SENDMESSAGE,  "/sendMessage").
+-define(FORWARDMESSAGE,  "/forwardMessage").
+
+
+getMe() ->
+  gen_server:cast(?MODULE, get_me).
+
+sendMessage() ->
+  gen_server:cast(?MODULE, send_message).
+
+forwardMessage() ->
+  gen_server:cast(?MODULE, forward_message).
+
+getUpdates() ->
+  gen_server:cast(?MODULE, getUpdates).
+
+init([]) ->  
+  io:format("~n~nAceptor Start~n~n"),     
+
+    {ok, []}.     
+
+
+handle_call(_Msg, _From, State) -> 
+
+
+    {reply, _From, State}.
+
+
+handle_cast(Msg, State) ->   
+    Method = case Msg of
+      get_me -> ?GETME;
+      send_message -> ?SENDMESSAGE;
+      forward_message -> ?FORWARDMESSAGE;
+      getUpdates -> ?GETUPDATES;
+      _ -> io:format("Unknown Comand"), <<"">>
+    end,
+
+    Url = ?HOST ++ ?TOKEN ++ Method,
+    %Body = <<"{\"chat_id\":\"93906430\",\"text\":\"93906430\"}">>,
+    Body = <<"{\"offset\":\"423321873\"}">>,
+
+    io:format("~p~n", [Body]),
+    %{ok,{{"HTTP/1.1",200,"OK"}, _, Body1}} = 
+    %Rez = httpc:request(post, {Url, [],"application/json",Body},[],[]),
+
+    {ok,{{"HTTP/1.1",200,"OK"}, _, Rez}} = httpc:request(post, {Url, [], "application/json",Body}, [], []),
+    io:format("request result~p~n", [jsx:decode(unicode:characters_to_binary(Rez))]),
+
+    {noreply, State}.                   
+
+
+handle_info({get_me, Method}, State) ->  
+  
+    Url = ?HOST ++ ?TOKEN ++ Method,
+    {ok,{{"HTTP/1.1",200,"OK"}, _, Body}} = httpc:request(get, {Url, []}, [], []),
+    io:format("request ~p~n", [jsx:decode(unicode:characters_to_binary(Body))]),
+
+    {noreply, State}; 
+
+handle_info(_Info, State) ->    
+
+    {noreply, State}.          
+
+terminate(_Reason, _State) -> 
+
+    ok.                       
+
+code_change(_OldVsn, State, _Extra) -> 
+    {ok, State}.              
--- src/autorization_sup.erl
+++ src/autorization_sup.erl
@@ -31,11 +31,36 @@ start_link() ->
 %% Before OTP 18 tuples must be used to specify a child. e.g.
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
+	
 
-	io:format("!!!!!!!!!!!!!!!!~n~n"),
-	
-	
-    {ok, { {one_for_all, 0, 1}, []} }.
+    inets:start(),  
+    ssl:start(), 
+    httpc:set_options([{proxy, {{"proxy.pbank.com.ua", 8080}, ["localhost"]}}]),
+	io:format("~n~nSupervisor Start~n~n"),
+
+	SupSpec =  {          % Global supervisor options
+      one_for_all,        % - use the one-for-one restart strategy
+      100,               % - and allow a maximum of 1000 restarts
+      300                % - per hour for each child process
+    },
+
+     ChildSpec = [         % The list of child processes you should supervise
+        {   
+        	aceptor,     % - Register it under the name hello_server
+	        {                 % - Here's how to find and start this child's code 
+		        aceptor,   %   * the module is called hello_server
+		        start_link,     %   * the function to invoke is called start_link
+		        []              %   * and here's the list of default parameters to use
+	        },                
+		        permanent,        % - child should run permantenly, restart on crash 
+		        2000,             % - give child 2 sec to clean up on system stop, then kill 
+		        worker,           % - FYI, this child is a worker, not a supervisor
+		        [aceptor]    % - these are the modules the process uses  
+	      	}],
+
+
+
+    {ok, {SupSpec, ChildSpec}}.
 
 %%====================================================================
 %% Internal functions
